fce6b62b81813be9a240d5ffe45ee0f729a31c80
Joane Pouech <70484783+JoanePouech@users.noreply.github.com>
2021-05-24 01:32:57 +0200

    Add JS orderPage

---
+++ src/js/orderPage.js
@@ -0,0 +1,15 @@
+/* Page 4 - Confirmation de commande */
+
+document.addEventListener ('DOMContentLoaded', (event) => {
+    printOrderInfos();
+});
+
+// FONCTION - Actualiser le montant total et le numéro de commande
+function printOrderInfos () {
+    // Montant total
+    let totalPrice = localStorage.getItem("totalPrice");
+    document.getElementById("totalPrice").innerText = totalPrice;
+    // Numéro de commande
+    let orderId = localStorage.getItem("orderId");
+    document.getElementById("orderId").innerText = orderId;
+}
